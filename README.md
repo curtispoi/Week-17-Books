d3f5cdba232e14e623419ff352a43f47356fc816
Curtis Poirrier <curtis.poirrier@gmail.com>
2022-04-20 21:18:21 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+## Library Catalog
+##### Title:
+  Library Catalog
+### Description: 
+  This project uses react and JSON to present a library catalog of coding books
+
+##### How to Run: 
+  download repository and launch server from directory
+ 
+##### License information: 
+  MIT license. 
---
+++ books.js
@@ -0,0 +1,47 @@
+function Book({data}){
+    let id = data.title.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(" ","");
+    const target = "#" + id;
+    return (
+
+        <div className="card">
+            <h5 className="card-header">{data.title}</h5>
+            <div className="card-body">
+            <h5>{data.subtitle}</h5>
+
+                <table className="table">
+                  <tbody>
+                    <tr>
+                        <td className="text-success font-weight-bold">Title:</td>
+                        <td>{data.title}</td>
+                    </tr>
+                    <tr>
+                        <td className="text-success font-weight-bold">Subtitle:</td>
+                        <td>{data.subtitle}</td>
+                    </tr>
+                    <tr>
+                        <td className="text-success font-weight-bold">Author:</td>
+                        <td>{data.author}</td>
+                    </tr>
+                    <tr>
+                        <td className="text-success font-weight-bold">Publisher:</td>
+                        <td>{data.publisher}</td>
+                    </tr>
+                    <tr>
+                        <td className="text-success font-weight-bold">Description:</td>
+                        <td>
+                            <a id={id} data-bs-toggle="collapse" href={target} role="button" aria-expanded="false" aria-controls="collapseExample">
+                                See More...
+                            </a>
+                            <div className="collapse" id={id}>
+                                {data.description}
+                            </div>
+                        </td>
+                        
+                    </tr>
+                  </tbody>
+                </table>
+
+            </div>
+        </div>
+
+)}
